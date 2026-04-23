7fab4de6db17d8cec257542d71b80975faa0cdd0
filthynobleman <maggioli.filippo@gmail.com>
2026-04-23 11:18:57 +0200

    Added single-inclusion header for simplicity. Fixed compilation issue under Linux systems.

---
+++ include/cescg/cescg.hpp
@@ -0,0 +1,28 @@
+/**
+ * @file        cescg.hpp
+ * 
+ * @brief       
+ * 
+ * @author      Filippo Maggioli (maggioli.filippo@gmail.com)
+ * 
+ * @date        2026-04-23
+ */
+#pragma once
+
+// Utilities
+#include <cescg/utils.hpp>
+#include <cescg/randgen.hpp>
+
+// Rendering and support structures
+#include <cescg/grid.hpp>
+#include <cescg/image.hpp>
+#include <cescg/rasterizer.hpp>
+
+// Basic geometry
+#include <cescg/bbox.hpp>
+#include <cescg/polygon.hpp>
+#include <cescg/halfplane.hpp>
+
+// Voronoi stuff
+#include <cescg/vororegion.hpp>
+#include <cescg/voronoi.hpp>
